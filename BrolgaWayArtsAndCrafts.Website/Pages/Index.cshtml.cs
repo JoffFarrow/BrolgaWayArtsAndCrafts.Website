2717bd7c26eba7a164a19b2f03a6a7985490e629
Jonathan Farrow <92287523+JoffFarrow@users.noreply.github.com>
2023-02-02 15:38:06 +1100

    Add project files.

---
+++ BrolgaWayArtsAndCrafts.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using BrolgaWayArtsAndCrafts.Website.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BrolgaWayArtsAndCrafts.Website.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+		public JsonFileProductsService ProductService;
+		public IEnumerable<Product> Products { get; private set; }
+
+		public IndexModel(ILogger<IndexModel> logger,
+			JsonFileProductsService productService)
+		{
+			_logger = logger;
+			ProductService = productService;
+		}
+
+		public void OnGet()
+		{
+			Products = ProductService.GetProducts();
+		}
+	}
+}
---
+++ BrolgaWayArtsAndCrafts.Website/Services/JsonFileProductsServices.cs
@@ -0,0 +1,34 @@
+﻿using BrolgaWayArtsAndCrafts.Website.Models;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+
+namespace ContosoCrafts.WebSite.Services
+{
+	public class JsonFileProductsService
+	{
+		public JsonFileProductsService(IWebHostEnvironment webHostEnvironment)
+		{
+			WebHostEnvironment = webHostEnvironment;
+		}
+
+		public IWebHostEnvironment WebHostEnvironment { get; }
+
+		private string JsonFileName
+		{
+			get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+		}
+
+		[return: MaybeNull]
+		public IEnumerable<Product> GetProducts()
+		{
+			using (var jsonFileReader = File.OpenText(JsonFileName))
+			{
+				return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+					new JsonSerializerOptions
+					{
+						PropertyNameCaseInsensitive = true
+					});
+			}
+		}
+	}
+}
