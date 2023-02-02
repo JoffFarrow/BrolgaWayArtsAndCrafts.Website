d5c8a8d19ea0710c477ec13788d19305f5cffc01
Jonathan Farrow <92287523+JoffFarrow@users.noreply.github.com>
2023-02-03 10:13:06 +1100

    Added controller

---
+++ BrolgaWayArtsAndCrafts.Website/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using BrolgaWayArtsAndCrafts.Website.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrolgaWayArtsAndCrafts.Website.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductsService productsService)
+        {
+            ProductsService = productsService;
+        }
+        public JsonFileProductsService ProductsService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductsService.GetProducts();
+        }
+    }
+}
--- BrolgaWayArtsAndCrafts.Website/Program.cs
+++ BrolgaWayArtsAndCrafts.Website/Program.cs
@@ -1,30 +1,15 @@
-using ContosoCrafts.WebSite.Services;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddTransient<JsonFileProductsService>();
-
-var app = builder.Build();
-IConfiguration configuration = app.Configuration;
-IWebHostEnvironment environment = app.Environment;
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace ContosoCrafts.WebSite
 {
-	app.UseExceptionHandler("/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
